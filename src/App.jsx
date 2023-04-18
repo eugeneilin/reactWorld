51758ad9a2e84d2185ad63fa0139f1f38a286874
Eugene Ilin <eugenepianist@gmail.com>
2023-04-18 11:49:00 -0400

    refactored app into function component

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FilmsList from './components/filmsList';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: ['ready', 'set', 'GO'],
-      text: '',
-    };
+function App(props) {
+  const [list, setList] = useState(['ready', 'set', 'GO']);
+  const [text, setText] = useState('');
 
-    this.onSubmit = this.onSubmit.bind(this);
-  }
-
-  onSubmit(e) {
+  function onSubmit(e) {
     e.preventDefault();
 
-    let updatedList = [...this.state.list, this.state.text];
+    let updatedList = [...list, text];
 
-    this.setState({
-      list: updatedList,
-      text: '',
-    });
+    setList(updatedList);
+    setText('');
   }
 
-  render() {
-    return (
-      <main>
-        <h1>ToDos Reactor</h1>
-        <form onSubmit={this.onSubmit}>
-          <div className='form-group'>
-            <input
-              type='text'
-              value={this.state.text}
-              onChange={(e) => this.setState({ text: e.target.value })}
-            />
-            <button type='submit'>Add</button>
-          </div>
-        </form>
-        <ul>
-          {this.state.list.map((item, idx) => (
-            <li key={item + idx}>{item}</li>
-          ))}
-        </ul>
-        {/* FilmsList */}
-        <h2>Films List Exercise</h2>
-        <p>Fetch API</p>
-        <FilmsList />
-      </main>
-    );
-  }
+  return (
+    <main>
+      <h1>ToDos Reactor</h1>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <input type='text' value={text} onChange={(e) => setText(e.target.value)} />
+          <button type='submit'>Add</button>
+        </div>
+      </form>
+      <ul>
+        {list.map((item, idx) => (
+          <li key={item + idx}>{item}</li>
+        ))}
+      </ul>
+
+      {/* FilmsList */}
+      <h2>Films List Exercise</h2>
+      <p>Fetch API</p>
+      <FilmsList />
+    </main>
+  );
 }
 
 export default App;
